d18ff3c95c95cac65e566b2914b44c02cc5ef64a
soorough <souravhpateriyad04@gmail.com>
2024-04-25 14:44:31 +0530

    issue:deployment issue

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
 require('dotenv').config();
-const express = require("express");
-const cors = require("cors");
-const rootRouter = require("./routes/index");
-import path from 'path';
+const express = require('express');
+// const cors = require('cors');
+const rootRouter = require('./routes/index');
+const path = require('path');
 
 const app = express();
-PORT = process.env. PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.use(
-//   cors({
-//     origin: ['http://localhost:3000','https://figr-assignment.vercel.app', 'https://papaya-dasik-127f0d.netlify.app'],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
+// // Configure CORS middleware
+// app.use(cors({
+//     origin: ['http://localhost:3000', 'https://figr-assignment.vercel.app', 'https://papaya-dasik-127f0d.netlify.app'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
 
-const __dirname = path.resolve();
-
-
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", rootRouter);
+// Set the root route for the API
+app.use('/api/v1', rootRouter);
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
+// Serve static files
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
+// Catch-all route for handling other routes and sending index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-})
-
-app.listen(PORT || 3000, function (err) {
-  if (err) console.log(err);
-  console.log(`Server listening to ${PORT}`);
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Start the server and listen on the specified port
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is listening on port ${PORT}`);
+    }
+});
