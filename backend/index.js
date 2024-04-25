badea3ec9bfda6c8f02637ef47803b87eaed3188
soorough <souravhpateriyad04@gmail.com>
2024-04-25 12:52:10 +0530

    fix: typo

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,12 @@ const rootRouter = require("./routes/index");
 const app = express();
 PORT = process.env. PORT || 3000;
 
-
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000','https://figr-assignment.vercel.app/'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
