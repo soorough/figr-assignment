b9d1a1e77c6cf06d1b160a6319b1d1276ddf4395
soorough <souravhpateriyad04@gmail.com>
2024-04-25 13:57:46 +0530

    issue:deployment issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': 'https://figr-assignment-soorough.onrender.com'
+    },
+  },
   plugins: [react()],
 })
